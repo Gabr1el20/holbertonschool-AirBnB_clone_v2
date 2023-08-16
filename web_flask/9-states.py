ede7b40a16260a2c73e11d32724d1a4bd797ca35
Gabr1el20 <6327@holbertonstudents.com>
2023-08-16 11:23:52 -0700

    Just a test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"Task 9. Script that starts a web Application"
+"""Task 9. Script that starts a Flask web application.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,28 +10,26 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def app_teardown_appcontext(self):
-    """Remove the current
-    SQLAlchemy session"""
+    """After each request, remove the current SQLAlchemy Session
+    """
     storage.close()
 
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """Generate a list of all the states"""
-    return render_template("9-states.html",
+    """Display a HTML page with the list of all States. Route ("/states")
+    """
+    return render_template("7-states_list.html",
                            states=storage.all(State).values())
 
 
-@app.route("/states/<string:id>", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """ Display the list of states, and
-    their cities if the id is displayed"""
-    for s in storage.all(State).values():
-        if s.id == id:
-            return render_template("9-states.html", id=id,
-                                   state=s, status='id')
-    return render_template("9-states.html", states=s, status='none')
+    """Display a HTML page of a specific state by id. Route ("/states/<id>")
+    """
+    return render_template("9-states.html",
+                           states=storage.all(State).get(f'State.{id}'))
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
